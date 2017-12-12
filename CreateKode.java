80811a6bd9346807181e3f20f9840a44dab96f27
Dwisantosow <32090298+Dwisantosow@users.noreply.github.com>
2017-12-12 10:02:37 +0700

    Add files via upload

---
+++ CreateKode.java
@@ -0,0 +1,12 @@
+public class CreateKode {
+
+    String tahun_ajaran = "2017-2018";
+    String semester = "Gasal";
+    String kd_prodi = "153";
+    String kode = "";
+
+    public void set_kode() {
+        //statements
+        //output kode = "1718115301"
+    }
+}
